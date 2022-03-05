ca4785b8a760f8c700b789df7fd4237e5149e52d
Bowen Zhang <b349zhan@Bowens-MacBook-Air.local>
2022-03-04 22:14:37 -0500

    setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB1TI4wIQ_3opkaDX5FbSeWw4fvXK67k4E",
+    authDomain: "b349zhan-ecommerce.firebaseapp.com",
+    projectId: "b349zhan-ecommerce",
+    storageBucket: "b349zhan-ecommerce.appspot.com",
+    messagingSenderId: "336902596853",
+    appId: "1:336902596853:web:53644683c6989b454ffdbc",
+    measurementId: "G-16GHY3L0LM"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB1TI4wIQ_3opkaDX5FbSeWw4fvXK67k4E",
+    authDomain: "b349zhan-ecommerce.firebaseapp.com",
+    projectId: "b349zhan-ecommerce",
+    storageBucket: "b349zhan-ecommerce.appspot.com",
+    messagingSenderId: "336902596853",
+    appId: "1:336902596853:web:53644683c6989b454ffdbc",
+    measurementId: "G-16GHY3L0LM"
+  }
 };
 
 /*
